c03ba6b2b24b4e9076f185f4b997b5dd8e5dd8b4
gunnarguy <gunnarguy@me.com>
2025-06-25 12:35:40 -0700

    Initial Commit

---
+++ LifeLapse/LifeLapseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeLapseApp.swift
+//  LifeLapse
+//
+//  Created by Gunnar Hostetler on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct LifeLapseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
